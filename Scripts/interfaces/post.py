85866a198138238a75850f8f40be00774ef225a9
Oscar Esteban <oesteban@die.upm.es>
2012-10-24 00:08:54 -0700

    Creation of packages for python A basic package structure is created

--- Scripts/interfaces/post.py
+++ Scripts/interfaces/post.py
@@ -1,7 +1,7 @@
 from nipype.interfaces.base import (InputMultiPath, OutputMultiPath, BaseInterface,
-                                    BaseInterfaceInputSpec, traits, File)
+                                    BaseInterfaceInputSpec, TraitedSpec, traits, File)
 from nipype.utils.filemanip import split_filename
-from ..util import postproc as pp
+import Scripts.util.postproc as pp
 
 import nibabel as nib
 import numpy as np
@@ -22,33 +22,24 @@ class PVProcessOutputSpec(TraitedSpec):
 class PVProcess(BaseInterface):
     input_spec = PVProcessInputSpec
     output_spec = PVProcessOutputSpec
+    _fnames = []
 
     def _run_interface(self, runtime):
         params = pp.loadParameters(self.inputs.parameters)
-        out_names = pp.fusePV( self.inputs.in_files,
+
+        _, base, _ = split_filename( self.inputs.in_maps[0] )
+
+        self._fnames = pp.fusePV( self.inputs.in_files,
                                self.inputs.in_maps,
-                               params,:
+                               params,
                                self.inputs.distance,
                                self.inputs.reorder,
-                               os.path.abspath( os.path.join( os.getcwd() ) )
-        fname = self.inputs.volume
-        img = nb.load(fname)
-        data = np.array(img.get_data())
-
-        active_map = data > self.inputs.threshold
-
-        thresholded_map = np.zeros(data.shape)
-        thresholded_map[active_map] = data[active_map]
-
-        new_img = nb.Nifti1Image(thresholded_map, img.get_affine(), img.get_header())
-        _, base, _ = split_filename(fname)
-        nb.save(new_img, base + '_thresholded.nii')
+                               os.path.abspath( base ) )
 
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        fname = self.inputs.volume
-        _, base, _ = split_filename(fname)
-        outputs["thresholded_volume"] = os.path.abspath(base + '_thresholded.nii')
+        _, base, _ = split_filename( self.inputs.in_maps[0] )
+        outputs["out_files"] = [ '%s_mseg%02d.nii.gz' % ( base, i ) for i in range(0,len(self.inputs.pure_tissues)) ]
         return outputs
